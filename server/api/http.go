5b7cfd24a31517fba8c105ea2645ccd6fc637655
JaydenNikifork <jayden.nikifork@gmail.com>
2024-11-25 16:09:24 +0000

    update allowed methods

--- server/api/http.go
+++ server/api/http.go
@@ -90,7 +90,7 @@ func CorsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 		// PROD CHECK
 		w.Header().Set("Access-Control-Allow-Origin", allowedOrigins[0])
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Methods", "GET")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		// Handle preflight OPTIONS request
